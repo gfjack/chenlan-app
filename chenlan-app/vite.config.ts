914686b0cc91127781d3f24060a72010081e3b7b
jack <gfjack321@gmail.com>
2024-11-29 11:58:20 +0800

    all

--- chenlan-app/vite.config.ts
+++ chenlan-app/vite.config.ts
@@ -8,7 +8,6 @@ import EnvironmentPlugin from 'vite-plugin-environment'
 export default defineConfig({
   build: {
     sourcemap: true,
-    base:'/tree/gh-pages/',
   },
   plugins: [react(), EnvironmentPlugin(['REACT_APP_TEXT'])],
   publicDir: 'public',
